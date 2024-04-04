2f10b1f45147b85b73b74a14290d96b9945a9fd2
Marco Lehmann <m99@posteo.de>
2024-04-04 16:45:41 +0200

    add Preact island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://m99io-astro-blog-tutorial.netlify.app',
+  integrations: [preact()],
 })
